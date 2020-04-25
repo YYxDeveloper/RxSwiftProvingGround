32ed35b126711c97a7e81ee9c51e7cb1fb70b25f
呂子揚 <yyxdev@gmail.com>
2020-04-25 14:49:01 +0800

    exampleTheObserver

--- RxSwiftProvingGround/ViewController.swift
+++ RxSwiftProvingGround/ViewController.swift
@@ -15,13 +15,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var yellowBtn: UIButton!
     var disposeBag = DisposeBag()
     let timer = Observable<Int>.interval(0.1, scheduler: MainScheduler.instance)
+    let numbers: Observable<Int> = Observable.create { observer -> Disposable in
+        
+        observer.onNext(0)
+        observer.onNext(1)
+        observer.onCompleted()
+        
+        return Disposables.create()
+    }
     
+    let theObserver: AnyObserver<Int> = AnyObserver { (event) in
+        switch event {
+        case .next(let data):
+            print(data)
+        case .error(let error):
+            print("Data Task Error: \(error)")
+        default:
+            break
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         
+    }
+    func exampleTheObserver() {
+        numbers
+            .subscribe(theObserver)
+            .disposed(by: disposeBag)
     }
     func exampleObservableBindUI()  {
         timer.map{ String(format: "%0.2d:%0.2d.%0.1d",
